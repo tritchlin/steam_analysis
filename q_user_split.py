192c057a3924d46d33d736925f4e520dc53f9415
Ryndine <rcmangeno@gmail.com>
2021-09-08 20:43:59 -0700

    Fixing indices file, add drop indices file

--- q_user_split.py
+++ q_user_split.py
@@ -2,13 +2,15 @@ from q_functions_split import db_interface, db_query
 
 steamdb = db_interface('steamdata.db')
 # input sql query file
-steamdb.set_query(path='player_loc_games.sql')
+# steamdb.set_query(path='player_loc_games.sql')
 # use one of these if you want to return a csv, or json. Then run file as is.
 # For memory issues, use parameter "chunk=number" example: steamdb.get_csv(chunk=1000)
 # print(steamdb.get_df())
 # steamdb.get_csv()
 # steamdb.get_json()
 steamdb.exec_script('create_indices.sql')
+# steamdb.exec_script('drop_indices.sql')
+
 
 # testing stuff.
 # q = query('all_apps')
